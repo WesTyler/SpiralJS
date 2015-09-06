c1157644703d66a45873e28d8c240c919e0f01d3
WesTyler <westyler5@gmail.com>
2015-09-06 15:29:24 -0500

    Draw line graph for typical signal/time plot

--- spiral.js
+++ spiral.js
@@ -15,55 +15,85 @@ function cartesian(radius, angle, size) {
   return [x, y, size];
 }
 
-var width = 800;
-var height = 800; 
+var width = 500;
+var height = 500; 
 
-var x = d3.scale.linear().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+var x = d3.scale.linear().range([0, width-10]);
+var y = d3.scale.linear().range([height-10, 0]);
 
 
-var svg = d3.select("body")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
+// var svg = d3.select("body")
+//   .append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//   .append("g")
 
-var data = []
+var data = [];
+var data2 = [];
 for (var i=100; i<1001; i++) {
   var angle = theta(i, 100);
   var rad = radius(8, angle);
   var size = 1 + Math.random()*2.5;
   if (i % 10 === 0) {
-    size = 3.5 + Math.random()*2;
+    size = 3.5 + Math.random()*3;
   }
   data.push(cartesian(rad, angle, size))
+  data2.push([i, size*100, 2])
 }
 
 y.domain([-height, height]);
 x.domain([-width, width]);
 
-var xAxis = d3.svg.axis().scale(x)
+// svg.selectAll("dot")
+//   .data(data)
+//     .enter().append("circle")
+//       .attr("r", function(d) { return d[2]; })
+//       .attr("cx", function(d) { return x(d[0]); })
+//       .attr("cy", function(d) { return y(d[1]); });
+
+// --------------------vvv Standard Line Graph vvv---------------------------
+
+var x2 = d3.scale.linear().range([0, width]);
+var y2 = d3.scale.linear().range([height, 0]);
+x2.domain(d3.extent(data2, function(d) { return d[0]; }));
+y2.domain(d3.extent(data2, function(d) { return d[1]; }));
+
+var xAxis = d3.svg.axis().scale(x2)
   .orient("bottom").ticks(5);
 
-var yAxis = d3.svg.axis().scale(y)
+var yAxis = d3.svg.axis().scale(y2)
   .orient("left").ticks(5);
 
-svg.selectAll("dot")
-  .data(data)
-    .enter().append("circle")
-      .attr("r", function(d) { return d[2]; })
-      .attr("cx", function(d) { return x(d[0]); })
-      .attr("cy", function(d) { return y(d[1]); });
+var line = d3.svg.line()
+  .x(function(d) { return x2(d[0]); })
+  .y(function(d) { return y2(d[1]); });
+
+var svg2 = d3.select("body")
+  .append("svg")
+    .attr("width", width+25)
+    .attr("height", height+25)
+  .append("g")
 
     // Add the X Axis
-svg.append("g")
+svg2.append("g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + height + ")")
   .call(xAxis);
 
     // Add the Y Axis
-svg.append("g")
+svg2.append("g")
   .attr("class", "y axis")
-  .call(yAxis);
+  .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
 
-console.log('theta:', theta(5, 3), 'r:', radius(0.5, theta(5, 3)), '[x,y]:', cartesian(radius(0.5, theta(5, 3)), theta(5, 3)))
+svg2.append("path")
+  .datum(data2)
+  .attr("class", "line")
+  .attr("d", line)
+  .attr("fill", 'none')
+  .attr('stroke-width', '1')
+  .attr('stroke', 'black')
