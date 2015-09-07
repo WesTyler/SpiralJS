9a47beb12bc505152b6c2ebb079e61d7f91c3bff
WesTyler <westyler5@gmail.com>
2015-09-07 10:29:11 -0500

    Draw spiral with paths instead of arcs

--- spiral.js
+++ spiral.js
@@ -1,5 +1,15 @@
 function spiral(type) { 
   var type = type || "points";
+
+  var margin = {top: 20, right: 20, bottom: 20, left: 40};
+  var width = 750 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom; 
+
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+  y.domain([-height, height]);
+  x.domain([-width, width]);
+
   // For time point #t, calculate the angle to the center of the arc
   // period is the number of time points per revolution (2 PI)
   function theta(t, period) {
@@ -20,17 +30,10 @@ function spiral(type) {
 
   function cartesian(radius, angle, size, startAngle, endAngle) {
     var size = size || 1;
-    var x = radius * Math.cos(angle);
-    var y = radius * Math.sin(angle);
-    return [x, y, size, radius, angle, startAngle, endAngle];
+    var xPos = x(radius * Math.cos(angle));
+    var yPos = y(radius * Math.sin(angle));
+    return [xPos, yPos, size, radius, angle, startAngle, endAngle];
   }
-
-  var margin = {top: 20, right: 20, bottom: 20, left: 40};
-  var width = 750 - margin.left - margin.right;
-  var height = 500 - margin.top - margin.bottom; 
-
-  var x = d3.scale.linear().range([0, width]);
-  var y = d3.scale.linear().range([height, 0]);
 
 
   var svg = d3.select("body")
@@ -51,8 +54,6 @@ function spiral(type) {
     data2.push([i, size*100, 2])
   }
 
-  y.domain([-height, height]);
-  x.domain([-width, width]);
   if (type === "points") {
     svg.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -61,8 +62,8 @@ function spiral(type) {
       .data(data)
         .enter().append("circle")
           .attr("r", function(d) { return d[2]; })
-          .attr("cx", function(d) { return x(d[0]); })
-          .attr("cy", function(d) { return y(d[1]); });
+          .attr("cx", function(d) { return d[0]; })
+          .attr("cy", function(d) { return d[1]; });
   } else if (type === "arcs") {
     svg.append("g")
       .attr("transform", "translate(" + (width * 0.5 + margin.left) + "," + (height * 0.5 + margin.top) + ")");
@@ -71,16 +72,98 @@ function spiral(type) {
       .data(data)
         .enter().append("path")
         .attr("d", d3.svg.arc()
-        .outerRadius(function(d){return d[3]+10})
-        .innerRadius(function(d){return d[3]-10})
-        .startAngle(function(d) { return d[5]; })
+        .outerRadius(function(d){return y(d[3]+10)})
+        .innerRadius(function(d){return y(d[3]-10)})
+        .startAngle(function(d) { return d[5] > 0 ? d[5] : 0; })
         .endAngle(function(d) { return d[6]; }))
         .attr("opacity", function(d){return d[2]/5})
+  } else if (type === "paths") {
+    
+    var line = d3.svg.line()
+        .interpolate("basis");
+
+    svg.append("g")
+      .attr("transform", "translate(" + (margin.left) + "," + (margin.top) + ")");
+      
+    svg.selectAll("g").selectAll("path")
+        .data(quad(sample(line(data), 8)))
+      .enter().append("path")
+        .style("fill", function(d) { return "none"; })
+        .style("stroke", function(d) { return "black"; })
+        .style("stroke-width", 0.5)
+        // .style("fill", function(d) { return color(d.t); })
+        // .style("stroke", function(d) { return color(d.t); })
+        .attr("d", function(d) { return lineJoin(d[0], d[1], d[2], d[3], 32); });
+
+    // Sample the SVG path string "d" uniformly with the specified precision.
+    function sample(d, precision) {
+      var path = document.createElementNS(d3.ns.prefix.svg, "path");
+      path.setAttribute("d", d);
+
+      var n = path.getTotalLength(), t = [0], i = 0, dt = precision;
+      while ((i += dt) < n) t.push(i);
+      t.push(n);
+
+      return t.map(function(t) {
+        var p = path.getPointAtLength(t), a = [p.x, p.y];
+        a.t = t / n;
+        return a;
+      });
+    }
+
+    // Compute quads of adjacent points [p0, p1, p2, p3].
+    function quad(points) {
+      return d3.range(points.length - 1).map(function(i) {
+        var a = [points[i - 1], points[i], points[i + 1], points[i + 2]];
+        a.t = (points[i].t + points[i + 1].t) / 2;
+        return a;
+      });
+    }
+
+    // Compute stroke outline for segment p12.
+    function lineJoin(p0, p1, p2, p3, width) {
+      var u12 = perp(p1, p2),
+          r = width / 2,
+          a = [p1[0] + u12[0] * r, p1[1] + u12[1] * r],
+          b = [p2[0] + u12[0] * r, p2[1] + u12[1] * r],
+          c = [p2[0] - u12[0] * r, p2[1] - u12[1] * r],
+          d = [p1[0] - u12[0] * r, p1[1] - u12[1] * r];
+
+      if (p0) { // clip ad and dc using average of u01 and u12
+        var u01 = perp(p0, p1), e = [p1[0] + u01[0] + u12[0], p1[1] + u01[1] + u12[1]];
+        a = lineIntersect(p1, e, a, b);
+        d = lineIntersect(p1, e, d, c);
+      }
+
+      if (p3) { // clip ab and dc using average of u12 and u23
+        var u23 = perp(p2, p3), e = [p2[0] + u23[0] + u12[0], p2[1] + u23[1] + u12[1]];
+        b = lineIntersect(p2, e, a, b);
+        c = lineIntersect(p2, e, d, c);
+      }
+
+      return "M" + a + "L" + b + " " + c + " " + d + "Z";
+    }
+
+    // Compute intersection of two infinite lines ab and cd.
+    function lineIntersect(a, b, c, d) {
+      var x1 = c[0], x3 = a[0], x21 = d[0] - x1, x43 = b[0] - x3,
+          y1 = c[1], y3 = a[1], y21 = d[1] - y1, y43 = b[1] - y3,
+          ua = (x43 * (y1 - y3) - y43 * (x1 - x3)) / (y43 * x21 - x43 * y21);
+      return [x1 + ua * x21, y1 + ua * y21];
+    }
+
+    // Compute unit vector perpendicular to p01.
+    function perp(p0, p1) {
+      var u01x = p0[1] - p1[1], u01y = p1[0] - p0[0],
+          u01d = Math.sqrt(u01x * u01x + u01y * u01y);
+      return [u01x / u01d, u01y / u01d];
+    }
   }
 }
 
 spiral('points');
 spiral('arcs');
+spiral('paths');
 
 // --------------------vvv Standard Line Graph vvv---------------------------
 
