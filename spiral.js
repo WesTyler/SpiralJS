d1f68b36649f792fd828a8b88a5e1d46ad52b496
WesTyler <westyler5@gmail.com>
2015-10-14 21:19:22 -0500

    Move sample spiral drawings to index.html to clean out spiral.js

--- spiral.js
+++ spiral.js
@@ -184,30 +184,3 @@ function endAngle(t, period) {
 function radius(spacing, angle) {
   return spacing * angle;
 }
-
-var spiral1 = new Spiral('points');
-spiral1.setParam('numberOfPoints', 1000);
-spiral1.setParam('period', 100);
-spiral1.setParam('svgHeight', 500);
-spiral1.setParam('svgWidth', 750);
-spiral1.setParam('spacing', 8);
-spiral1.randomData();
-spiral1.render();
-
-var spiral2 = new Spiral('custom-path')
-spiral2.setParam('numberOfPoints', 1000);
-spiral2.setParam('period', 100);
-spiral2.setParam('svgHeight', 500);
-spiral2.setParam('svgWidth', 750);
-spiral2.setParam('spacing', 8);
-spiral2.setParam('lineWidth', 50);
-spiral2.randomData();
-spiral2.render();
-
-var spiral3 = new Spiral('non-spiral')
-spiral3.setParam('numberOfPoints', 1000);
-spiral3.setParam('period', 100);
-spiral3.setParam('svgHeight', 500);
-spiral3.setParam('svgWidth', 750);
-spiral3.randomData();
-spiral3.render();
